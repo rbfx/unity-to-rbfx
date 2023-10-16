c5e39934f67768bb92d1318049b8b1d25e92c06b
Gleb <gleb@gleblebedev.com>
2023-10-16 21:45:26 +0100

    Export animated model with skeleton

--- Editor/MeshExporter.cs
+++ Editor/MeshExporter.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.IO;
 using System.Text;
 using System.Xml.Linq;
+using UnityEditor;
 using UnityEngine;
 
 namespace UnityToRebelFork.Editor
@@ -22,11 +23,35 @@ namespace UnityToRebelFork.Editor
         {
             yield return asset.name;
 
+            // Get asset path in the asset database
+            var assetPath = AssetDatabase.GetAssetPath(asset);
+
+            SkinnedMeshRenderer skinRenderer = null;
+            if (AssetDatabase.LoadMainAssetAtPath(assetPath) is GameObject gameObject)
+            {
+                SkinnedMeshRenderer renderer;
+                if (gameObject.TryGetComponent<SkinnedMeshRenderer>(out renderer) && renderer.sharedMesh == asset)
+                {
+                    skinRenderer = renderer;
+                }
+
+                else
+                {
+                    foreach (var renderer2 in gameObject.GetComponentsInChildren<SkinnedMeshRenderer>())
+                    {
+                        if (renderer2.sharedMesh == asset)
+                        {
+                            skinRenderer = renderer2;
+                        }
+                    }
+                }
+            }
+
             using (var stream = _settings.CreateFile(EvaluateResourcePath(asset)))
             {
                 using (var writer = new BinaryWriter(stream))
                 {
-                    new ModelBuilder(Settings).WriteMesh(writer, new MeshSource(asset));
+                    new ModelBuilder(Settings).WriteMesh(writer, new MeshSource(asset, skinRenderer));
                 }
             }
         }
